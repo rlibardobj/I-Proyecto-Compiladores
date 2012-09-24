ad2f919b4d67f6e7b603b6fe433d85253c9bef2d
Ronny <rlibardobj@gmail.com>
2012-09-23 20:47:43 -0600

    Añadida clase MulDeclAST.cs

---
+++ ASintactico/MulDeclAST.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: rlibardobj
+ * Date: 23/09/2012
+ * Time: 08:47 p.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace ASintactico
+{
+	/// <summary>
+	/// Description of MulDeclAST.
+	/// </summary>
+	public class MulDeclAST
+	{
+		public MulDeclAST()
+		{
+		}
+	}
+}
