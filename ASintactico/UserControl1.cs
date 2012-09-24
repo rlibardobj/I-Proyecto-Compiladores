b0c8469bad0e687043c5c62657936f6d337078e0
Harvey Sandí <HMSScr@gmail.com>
2012-09-23 20:59:50 -0600

    add declarationAST

---
+++ ASintactico/UserControl1.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: HMSS
+ * Date: 23/09/2012
+ * Time: 08:56 p.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ASintactico
+{
+	/// <summary>
+	/// Description of UserControl1.
+	/// </summary>
+	public partial class UserControl1 : UserControl
+	{
+		public UserControl1()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
