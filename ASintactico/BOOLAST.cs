053e9966ad8383ce50b910711d3b241eaa8304ed
Ronny <rlibardobj@gmail.com>
2012-09-30 15:06:26 -0600

    Problema con BOOLAST arreglado.

---
+++ ASintactico/BOOLAST.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: rlibardobj
+ * Date: 30/09/2012
+ * Time: 03:05 p.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace ASintactico
+{
+	/// <summary>
+	/// Description of BOOLAST.
+	/// </summary>
+	public class BOOLAST:TerminalesAST
+	{
+		string value;
+		public BOOLAST(string valor)
+		{
+			value=valor;
+		}
+	}
+}
